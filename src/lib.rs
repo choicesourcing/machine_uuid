dd4f325eb09b08927d37a7c021d362df979ee49c
NAlexandrov <nick.alexandrov@gmail.com>
2022-12-17 11:45:24 +0700

    feat: Retrieves raw format of macos UUID retrieval.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 mod machineid;
 
 pub use machineid::get;
-pub use machineid::get_via_windows_shell;
 pub use machineid::get_via_linux_shell;
+pub use machineid::get_via_macos_shell;
+pub use machineid::get_via_windows_shell;
 pub use machineid::transform_windows_output;
-
-
+pub use machineid::MachineIdError;
