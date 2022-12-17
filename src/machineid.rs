8ffaafc85718bc7e6132d82ba4e11d7aa44ce44e
NAlexandrov <nick.alexandrov@gmail.com>
2022-12-17 10:04:27 +0700

    chore(rustfmt): code style fixes

--- src/machineid.rs
+++ src/machineid.rs
@@ -3,8 +3,8 @@
 //! A library for retrieving UUID.
 
 pub use self::machineid::get;
-pub use self::machineid::get_via_windows_shell;
 pub use self::machineid::get_via_linux_shell;
+pub use self::machineid::get_via_windows_shell;
 pub use self::machineid::transform_windows_output;
 
 pub mod machineid {
@@ -17,19 +17,18 @@ pub mod machineid {
     ///
     /// ```
     /// let uuid = machine_uuid::get();
-    /// 
+    ///
     /// // Based on OS, UUID format will differ
     /// // Windows
     /// #[cfg(any(windows))]
     /// assert_eq!("140EF834-2DB3-0F7A-27B4-4CEDFB73167C", uuid);
-    /// 
+    ///
     /// // Based on OS, UUID format will differ
     /// #[cfg(any(linux))]
     /// assert_eq!("92cc698195f84d3b85f1cfb0a09e957f\n", uuid);
-    /// 
+    ///
     /// ```
-    pub fn get() -> String
-    {
+    pub fn get() -> String {
         let mut uuid = String::from("");
 
         if cfg!(windows) {
@@ -40,40 +39,36 @@ pub mod machineid {
         }
 
         return uuid;
-
     }
 
     /// Retrieves raw format of UUID retrieval.
-    /// 
+    ///
     /// # Examples
     ///
     /// ```
     /// #[cfg(any(windows))]
     /// let untransformed = machine_uuid::get_via_windows_shell();
-    /// 
+    ///
     /// // c:\ wmic csproduct get UUID
     /// // UUID
     /// // 140EF834-2DB3-0F7A-27B4-4CEDFB73167C
     ///
     /// ```
     pub fn get_via_windows_shell() -> String {
-
         let output = Command::new("cmd")
             .args(&["/C", "wmic csproduct get UUID"])
             .output()
             .expect("failed to execute process");
 
-        let result = match String::from_utf8(output.stdout) 
-                    {
-                        Ok(line) =>  line,
-                        Err(_) => {
-                            println!("Failed to retrieve UUID from windows shell.");
-                            String::from("")
-                        }
-                    };
+        let result = match String::from_utf8(output.stdout) {
+            Ok(line) => line,
+            Err(_) => {
+                println!("Failed to retrieve UUID from windows shell.");
+                String::from("")
+            }
+        };
 
         return result;
-
     }
 
     /// Retrieves raw format of UUID retrieval.
@@ -83,7 +78,7 @@ pub mod machineid {
     /// ```
     /// #[cfg(any(unix))]
     /// let uuid_untrimmed = machine_uuid::get_via_linux_shell();
-    /// 
+    ///
     /// // # cat /etc/machine-id
     /// // 92cc698195f84d3b85f1cfb0a09e957f
     ///
@@ -95,9 +90,8 @@ pub mod machineid {
             .output()
             .expect("failed to execute process");
 
-        let result = match String::from_utf8(output.stdout) 
-        {
-            Ok(line) =>  line,
+        let result = match String::from_utf8(output.stdout) {
+            Ok(line) => line,
             Err(_) => {
                 println!("Failed to retrieve UUID from linux shell.");
                 String::from("")
@@ -115,7 +109,7 @@ pub mod machineid {
     /// // Output
     /// // UUID  *transforms removes*
     /// // 140EF834-2DB3-0F7A-27B4-4CEDFB73167C *transform returns*
-    /// 
+    ///
     /// //let raw = machine_uuid::get_via_windows_shell();
     /// //let just_uuid = machine_uuid::transform_windows_output(raw);
     ///
@@ -135,16 +129,16 @@ mod tests {
 
     #[test]
     fn test_if_gets_uuid() {
-
         if cfg!(windows) {
             let output = machineid::get_via_windows_shell();
-            let result = transform_windows_output(output); 
-            let re = Regex::new(r"\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b").unwrap();
+            let result = transform_windows_output(output);
+            let re =
+                Regex::new(r"\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b")
+                    .unwrap();
             assert!(re.is_match(&result.to_lowercase()));
         } else {
             let result = machineid::get_via_linux_shell();
             assert!(result.trim().len() == 32);
         }
     }
-       
 }
